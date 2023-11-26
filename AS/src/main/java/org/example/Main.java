b64128a5753476eea67634d9f6dabb6352ece32b
alkuranm <t-malkoran@microsoft.com>
2023-11-26 16:53:41 +0200

    commit the main

---
+++ AS/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // the pathe of file and json
+        String Path2Json = "C:\\Users\\t-malkoran\\Desktop\\Assignment\\AS\\src\\main\\java\\org\\example\\tester.json";
+        String Path2File = "C:\\Users\\t-malkoran\\Desktop\\Assignment\\AS\\src\\main\\java\\org\\example\\moade.txt";
+
+        //Class JSON
+        Mapper json2Map = new Json2Map();
+        HashMap<String, String> Indicators2Domain= json2Map.mapper(Path2Json);
+        // GIT THE TYPEOFFILE
+        FileFactory file = new FileFactory();
+        ReadFile reader = file.getInstance(Path2File);
+
+        //
+        Classify classify = new SetFiller();
+        Set<String> foundDomainsSet = new HashSet<>();
+
+        boolean isTextFinished = false;
+        int i = 0;
+        while (!isTextFinished) {
+            String[] words = reader.read(Path2File, i);
+            if (words == null) {
+                isTextFinished = true;
+            } else {
+                classify.SetFill(foundDomainsSet, Indicators2Domain, words);
+            }
+            i++;
+        }
+
+        for (String item : foundDomainsSet) {
+            System.out.print(item + "  ");
+        }
+    }
+}
